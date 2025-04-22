024997c70d822d67dc10e087db8610e800b19412
Pozitronik <antikillerxxl@gmail.com>
2025-04-22 17:12:33 +0400

    Update parameter value to the fixed one

--- tests/handlers/test_base_frame_handler.py
+++ tests/handlers/test_base_frame_handler.py
@@ -6,7 +6,7 @@ from sinner.Parameters import Parameters
 from sinner.handlers.frame.BaseFrameHandler import BaseFrameHandler
 from sinner.handlers.frame.CV2VideoHandler import CV2VideoHandler
 from sinner.handlers.frame.DirectoryHandler import DirectoryHandler
-from sinner.handlers.frame.FFmpegVideoHandler import FFmpegVideoHandler
+from sinner.handlers.frame.FFMpegVideoHandler import FFMpegVideoHandler
 from sinner.handlers.frame.ImageHandler import ImageHandler
 from sinner.handlers.frame.VideoHandler import VideoHandler
 from tests.constants import target_mp4, state_frames_dir, target_png
@@ -16,7 +16,7 @@ parameters: Namespace = Parameters().parameters
 
 def test_handlers_factory() -> None:
     assert (BaseFrameHandler.create(handler_name='CV2VideoHandler', parameters=parameters, target_path=target_mp4), CV2VideoHandler)
-    assert (BaseFrameHandler.create(handler_name='FFmpegVideoHandler', parameters=parameters, target_path=target_mp4), FFmpegVideoHandler)
+    assert (BaseFrameHandler.create(handler_name='FFMpegVideoHandler', parameters=parameters, target_path=target_mp4), FFMpegVideoHandler)
     assert (BaseFrameHandler.create(handler_name='VideoHandler', parameters=parameters, target_path=target_mp4), VideoHandler)
     assert (BaseFrameHandler.create(handler_name='DirectoryHandler', parameters=parameters, target_path=state_frames_dir), DirectoryHandler)
     assert (BaseFrameHandler.create(handler_name='ImageHandler', parameters=parameters, target_path=target_png), ImageHandler)
