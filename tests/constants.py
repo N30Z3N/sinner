c1e49c2dfd34f791123721a5331a7036f766e046
Pozitronik <antikillerxxl@gmail.com>
2025-04-25 13:48:50 +0400

    Path constants changed

--- tests/constants.py
+++ tests/constants.py
@@ -27,8 +27,8 @@ result_target_png: str = os.path.join(data_targets_dir, 'result-target.png')  # 
 source_target_mp4: str = os.path.join(data_targets_dir, 'source-target.mp4')  # auto result name for video swap
 result_target_mp4: str = os.path.join(data_targets_dir, 'result-target.mp4')  # auto result name for video processing
 source_images_result: str = resolve_relative_path('data/source-images', __file__)  # auto result name for images swap
-result_frames: str = resolve_relative_path('data/result-frames', __file__)  # auto result name for frames processing
-source_frames: str = resolve_relative_path('data/source-frames', __file__)  # auto result name for frames swap
+result_frames: str = resolve_relative_path('data/frames/result-png', __file__)  # auto result name for frames processing
+source_frames: str = resolve_relative_path('data/frames/source-png', __file__)  # auto result name for frames swap
 tmp_dir: str = resolve_relative_path('temp', get_app_dir())
 
 state_frames_dir: str = resolve_relative_path('data/frames/png', __file__)
