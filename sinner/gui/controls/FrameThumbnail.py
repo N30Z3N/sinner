0d3cf55433d38095aaccb35be72bb531c2f59541
Pozitronik <antikillerxxl@gmail.com>
2025-04-26 15:01:52 +0400

    Update type hints due package changes

--- sinner/gui/controls/FrameThumbnail.py
+++ sinner/gui/controls/FrameThumbnail.py
@@ -17,14 +17,14 @@ class FrameThumbnail:
             setattr(self, key, value)
 
     def photo(self, size: tuple[int, int] = (-1, -1)) -> ImageTk.PhotoImage:
-        image: Image = Image.fromarray(cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB))
+        image: Image.Image = Image.fromarray(cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB))
         if size[0] < 0 or size[1] < 0:
             size = image.width // 10, image.height // 10
         image = self.resize_image(image, size)
         return ImageTk.PhotoImage(image)
 
     @staticmethod
-    def resize_image(image: Image, size: tuple[int, int]) -> Image:
+    def resize_image(image: Image.Image, size: tuple[int, int]) -> Image.Image:
         aspect_ratio = image.size[0] / image.size[1]
         new_width = size[0]
         new_height = int(size[0] / aspect_ratio)
